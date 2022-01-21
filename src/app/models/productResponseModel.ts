7e834c1d67ade031758437a681f6700d544fc94e
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2022-01-21 12:04:54 +0300

    ListResponseModel<T>

--- src/app/models/productResponseModel.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "./product";
-import { ResponseModel } from "./responseModel";
-
-export interface ProductResponseModel extends ResponseModel{
-    // Bana gelicek datayi karsilayacak model
-    data:Product[]
-    
-}
