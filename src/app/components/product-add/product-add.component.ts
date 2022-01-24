7a2d7f7e7d3816ec9c04d904fbe49d21666e3932
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2022-01-25 00:43:47 +0300

    products/add - ReactiveFormsModule

---
+++ src/app/components/product-add/product-add.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.css'],
+})
+export class ProductAddComponent implements OnInit {
+  productAddForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private toastrService: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.createProductAddForm();
+  }
+
+  createProductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      unitPrice: ['', Validators.required],
+      unitsInStock: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    });
+  }
+
+  addProduct() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value);
+      this.productService.addProduct(productModel).subscribe(
+        (response) => {
+          this.toastrService.success(response.message, 'Başarılı');
+        },
+        (responseError) => {
+          if (responseError.error.ValidationErrors.length > 0) {
+            console.log(responseError.error.ValidationErrors[0].ErrorMessage);
+            this.toastrService.error(responseError.error.ValidationErrors[0].ErrorMessage, 'HATA');
+          }
+        }
+      );
+    } else {
+      this.toastrService.error('Formunuz Eksik', 'HATA');
+    }
+  }
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,23 +3,33 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   hostUrl = 'https://localhost:44321/';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  getProducts() : Observable<ListResponseModel<Product>> {
-    let apiUrl = this.hostUrl + "api/products/getAllProducts";
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let apiUrl = this.hostUrl + 'api/products/getAllProducts';
     return this.httpClient.get<ListResponseModel<Product>>(apiUrl);
   }
 
-  getProductsByCategoryId(categoryId:number) : Observable<ListResponseModel<Product>> {
-    let apiUrl = this.hostUrl + "api/products/getAllProductsByCategoryId?categoryId="+categoryId;
+  getProductsByCategoryId(
+    categoryId: number
+  ): Observable<ListResponseModel<Product>> {
+    let apiUrl =
+      this.hostUrl +
+      'api/products/getAllProductsByCategoryId?categoryId=' +
+      categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(apiUrl);
   }
-  
+
+  addProduct(product: Product): Observable<ResponseModel> {
+    let apiUrl = this.hostUrl + 'api/products/addProduct';
+    return this.httpClient.post<ResponseModel>(apiUrl, product);
+  }
 }
